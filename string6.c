2f1d10a2c8bc85f7591770436c8a00e184d953eb
RDPavan <rathodPawan8530@gmail.com>
2025-11-08 23:29:45 +0530

    two defferent type of memory adress stored

---
+++ string6.c
@@ -0,0 +1,32 @@
+/*#include<stdio.h>
+int main(){
+        int i;
+        char sports[5][15]={
+            "golf",
+            "hockey",
+            "football",
+            "cricket",
+            "shooting"
+        };
+        for(i=0;i<5;i++){
+            printf("String =%10s",sports[i]);
+            printf("\t Address of string literal = %d\n",sports[i]);
+ 
+        }
+}*/
+#include<stdio.h>
+int main(){
+        int i;
+        char *sports[5]={
+            "golf",
+            "hockey",
+            "football",
+            "cricket",
+            "shooting"
+        };
+        for(i=0;i<5;i++){
+            printf("String =%10s",sports[i]);
+            printf("\t Address of string literal = %u\n",sports[i]);
+ 
+        }
+}
